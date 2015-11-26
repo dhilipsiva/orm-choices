627c6854e99a96d9dba12074ab3d74616c05fe99
dhilipsiva <dhilipsiva@gmail.com>
2015-11-26 14:53:05 +0530

    Addd docs and deploy scripts

---
+++ DESCRIPTION.rst
@@ -0,0 +1,8 @@
+A helpful decorator for choice fields (Django choices or SQLAlchemy ChoiceType)
+===============================================================================
+
+A helpful decorator for choice fields (Django choices or SQLAlchemy ChoiceType). Do choices the clean way.
+
+Please look at the `README.md <https://github.com/dhilipsiva/choices/blob/master/README.md/>`_.
+
+DOCS: https://github.com/dhilipsiva/choices/blob/master/README.md
---
+++ setup.py
@@ -0,0 +1,71 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# vim: fenc=utf-8
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+#
+
+"""
+File name: setup.py
+Version: 0.1
+Author: dhilipsiva <dhilipsiva@gmail.com>
+"""
+__author__ = "dhilipsiva"
+__status__ = "development"
+
+"""
+A helpful decorator for choice fields (Django choices or SQLAlchemy ChoiceType)
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+f = path.join(here, 'README.md')
+
+try:
+    from pypandoc import convert
+    long_description = convert(f, 'rst')
+except ImportError:
+    print(
+        "pypandoc module not found, could not convert Markdown to RST")
+    long_description = open(f, 'r').read()
+
+setup(
+    name='choices',
+    version='0.1.0',
+    description=(
+        "A helpful decorator for choice fields"
+        " (Django choices or SQLAlchemy ChoiceType)"),
+    long_description=long_description,
+    url='https://github.com/dhilipsiva/choices',
+    author='dhilipsiva',
+    author_email='dhilipsiva@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='django orm sqlalchemy choices',
+    packages=find_packages(),
+    py_modules=['choices'],
+    entry_points='''''',
+    install_requires=[],
+    extras_require={
+        'dev': [''],
+        'test': [''],
+    },
+)
